c76f24c877bbe4bf98bc84c40a86034034ef7788
Serhii Pimenov <serhii@pimenov.com.ua>
2025-03-01 08:21:04 +0200

    add model

--- source/model/index.js
+++ source/model/index.js
@@ -2,16 +2,88 @@ class Model {
     constructor(data = {}) {
         this.elements = [];
         this.inputs = [];
+        this.computed = {};
+
+        // Регистрируем вычисляемые свойства
+        for (const key in data) {
+            if (typeof data[key] === 'function') {
+                this.computed[key] = {
+                    getter: data[key],
+                    value: null,
+                    dependencies: [] // Будет заполнено при первом вызове
+                };
+            }
+        }
+        
         this.data = new Proxy(data, {
             set: (target, property, value) => {
                 target[property] = value;
                 this.updateDOM(property, value);
                 this.updateInputs(property, value);
+                this.updateComputedProperties(property);
                 return true;
+            },
+
+            get: (target, property) => {
+                // Обрабатываем доступ к вычисляемым свойствам
+                if (property in this.computed) {
+                    return this.evaluateComputed(property);
+                }
+
+                return target[property];
             }
         });
+
+        // Вычисляем начальные значения computed свойств
+        this.initComputedProperties();
     }
 
+    // Инициализация начальных значений вычисляемых свойств
+    initComputedProperties() {
+        for (const key in this.computed) {
+            this.evaluateComputed(key);
+        }
+    }
+
+    // Вычисление значения computed свойства
+    evaluateComputed(key) {
+        const computed = this.computed[key];
+
+        // Создаем контекст для сбора зависимостей
+        const dependencies = new Set();
+        const dataTracker = new Proxy(this.data, {
+            get: (target, prop) => {
+                dependencies.add(prop);
+                return target[prop];
+            }
+        });
+
+        // Вызываем геттер для вычисления значения и сбора зависимостей
+        const result = computed.getter.call(dataTracker);
+
+        // Сохраняем зависимости
+        computed.dependencies = [...dependencies];
+        computed.value = result;
+
+        return result;
+    }
+
+    // Обновление вычисляемых свойств при изменении зависимостей
+    updateComputedProperties(changedProp) {
+        for (const key in this.computed) {
+            const computed = this.computed[key];
+
+            // Если изменившееся свойство находится в зависимостях
+            if (computed.dependencies.includes(changedProp)) {
+                const newValue = this.evaluateComputed(key);
+
+                // Обновляем DOM для вычисляемого свойства
+                this.updateDOM(key, newValue);
+                this.updateInputs(key, newValue);
+            }
+        }
+    }
+    
     // Парсимо DOM для пошуку виразів {{ змінна }}
     parse(rootElement) {
         let root
@@ -45,7 +117,7 @@ class Model {
             while ((match = regex.exec(text)) !== null) {
                 const propName = match[1].trim();
 
-                if (propName in this.data) {
+                if (propName in this.data || propName in this.computed) {
                     this.elements.push({
                         node,
                         propName,
@@ -146,8 +218,11 @@ class Model {
         let result = template;
         const regex = new RegExp(`\\{\\{\\s*${propName}\\s*\\}\\}`, 'g');
 
+        // Определяем значение (обычное или вычисляемое свойство)
+        const propValue = propName in this.computed ? this.computed[propName].value : value;
+
         // Змінюємо всі входження {{ змінна }} на значення
-        result = result.replace(regex, value);
+        result = result.replace(regex, propValue);
 
         // Оновлюємо інші змінні в шаблоні, якщо вони є
         for (const key in this.data) {
