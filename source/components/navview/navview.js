ba981b286ce0a02c52c4ee49179d8fd6e164aeef
CmPi <CmPi@users.noreply.github.com>
2025-08-14 23:18:43 +0200

    Update navview.js
    non necessary log ?

--- source/components/navview/navview.js
+++ source/components/navview/navview.js
@@ -110,7 +110,6 @@
             const content = element.children(".navview-content");
             const toggle = $(o.toggle);
             menu = pane.children(".navview-menu");
-            console.log(menu);
             if (menu.length) {
                 menu.prevAll().reverse().wrapAll($("<div>").addClass("navview-container"));
                 menu.wrap($("<div>").addClass("navview-menu-container"));
